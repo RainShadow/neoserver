a7d31440ec2a851ad074e04d8eeeea44457a678a
Steve Heckt <home@u2me.com>
2016-02-11 20:59:46 -0800

    ...and didn't save my final change before that last commit

--- routes/update.js
+++ routes/update.js
@@ -1,7 +1,7 @@
 var spawnSync = require('child_process').spawnSync;
 
 module.exports = function(request, response) {
-  var ret = spawnSync('git', ['-C', '/Users/personal/Dropbox/projects/neoserver', 'pull']);
-  // var ret = spawnSync('git', ['-C', '/home/pi/neoserver', 'pull']);
+  // var ret = spawnSync('git', ['-C', '/Users/personal/Dropbox/projects/neoserver', 'pull']);
+  var ret = spawnSync('git', ['-C', '/home/pi/neoserver', 'pull']);
   response.end('Update says: '+ret.stdout+' '+ret.stderr);
 };
