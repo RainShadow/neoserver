6fb5a5acdc8fe83585f1e422cbe4171e908f9feb
Steve Heckt <home@u2me.com>
2016-02-11 20:58:15 -0800

    Greatly simplified routes/update.js by using child_process.spawnSync

--- routes/update.js
+++ routes/update.js
@@ -1,23 +1,7 @@
-function cmd_exec(cmd, args, cb_data, cb_end) {
-  var spawn = require('child_process').spawn;
-  var child = spawn(cmd, args);
-  var self = this;
-
-  self.exit = 0;  // Send a cb to set 1 when cmd exits
-  self.stdout = "";
-  child.stdout.on('data', function (data) { cb_data(self, data); });
-  child.stdout.on('end', function () { cb_end(self); });
-}
+var spawnSync = require('child_process').spawnSync;
 
 module.exports = function(request, response) {
-  foo = new cmd_exec('git', ['-C ~/neoserver', 'pull'],
-  // foo = new cmd_exec('ls', ['-ralt'],
-    function (obj, data) {
-      obj.stdout += data.toString();
-    },
-    function (obj) {
-      obj.exit = 1;
-      response.end('Update says: '+obj.stdout);
-    }
-  );
+  var ret = spawnSync('git', ['-C', '/Users/personal/Dropbox/projects/neoserver', 'pull']);
+  // var ret = spawnSync('git', ['-C', '/home/pi/neoserver', 'pull']);
+  response.end('Update says: '+ret.stdout+' '+ret.stderr);
 };
