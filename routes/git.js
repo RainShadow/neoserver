9778e5130e484b1b6ace2d0ce82b39991e8b899a
Steve Heckt <home@u2me.com>
2016-02-12 10:09:54 -0800

    Adding 'git fetch' before status so status returns accurate results

--- routes/git.js
+++ routes/git.js
@@ -3,8 +3,12 @@ var spawnSync = require('child_process').spawnSync;
 module.exports = function(router, route) {
   // fetches a list of files that have changed between this server and the remote master
   router.get(route, function(request, response) {
-    // var ret = spawnSync('git', ['-C', __dirname, 'diff', 'origin', '--stat']);
-    var ret = spawnSync('git', ['-C', __dirname, 'status']);
+    // need to to a fetch first so we get the latest meta data from our source repo
+    var ret = spawnSync('git', ['-C', __dirname, 'fetch']);
+    if (ret.stderr.length > 0) response.end('ERROR (on fetch): '+ret.stderr);
+    // okay, get the status (no changes made to server)
+    ret = spawnSync('git', ['-C', __dirname, 'status']);
+    // ret = spawnSync('git', ['-C', __dirname, 'diff', 'HEAD..origin', '--stat']);
     if (ret.stderr.length > 0) response.end('ERROR: '+ret.stderr);
     response.end(ret.stdout);
   });
